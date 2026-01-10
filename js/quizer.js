c9ede74c3bdeca7df897e8f8edb6b717175cab0c
KapitanMorkow <157925142+KapitanMorkow@users.noreply.github.com>
2026-01-10 06:25:49 +0300

    Add files via upload

--- js/quizer.js
+++ js/quizer.js
@@ -836,6 +836,11 @@ let ru_2010_f = [
 		pack : RU_2010_F_PACK_3,
 		group : 'Kiliana',
 		song : "Асфальт (ft Jakone) (2024)"
+	},
+	{
+		pack : RU_2010_F_PACK_3,
+		group : 'Never Get Used To People',
+		song : "Life Letters (2018)"
 	}
 ];
 
